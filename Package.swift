90d2599e72e738b71da1e8fb6347160c78a36ad0
Jonathan Guthrie <jono@perfect.org>
2016-09-14 13:38:56 -0400

    use correct package name

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 // Date: 2016-09-13 13:48:18 +0000
 import PackageDescription
 let package = Package(
-    name: "jsonConfig",
+    name: "JSONConfig",
     targets: [],
     dependencies: [
         .Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 2, minor: 0)
