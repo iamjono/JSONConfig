8cab181dadc3e25a5837bae6789c6d2d634909d2
Jonathan Guthrie <jono@perfect.org>
2017-06-21 14:04:07 -0400

    remove minor

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "JSONConfig",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 2, minor: 0)
+        .Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 2)
 	],
     exclude: []
 )
